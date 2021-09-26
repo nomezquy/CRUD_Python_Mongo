3aa86270004c580cb63e8b0bc0b670791fd58c26
Daniel <dnomezquy@gmail.com>
2021-09-26 18:44:28 -0500

    Cargando nuevos archivos adicionales

---
+++ ciclos.py
@@ -0,0 +1,32 @@
+#Ciclo while
+
+"""
+while condición:
+    ejecución tabulada.
+    incrementoo validación de condicion
+"""
+#ciclo for
+
+"""
+for [contador] in [tabla,diccionario,]:
+    ejecución tabulada.
+
+for [contador] in range([numero max])
+    ejecucion tabulada.
+
+"""
+
+# en cualquier momento se puede indicar un condicional y un break para cortar el ciclo.
+
+
+# For else
+# permite retornar en el else un valor si una condicion dentro del for no se cumple
+"""
+numeros = [1, 2, 4, 3, 5, 8, 6]
+for n in numeros:
+    if n == 3:
+        break
+else:
+    print('No se encontró el número 3')
+
+"""
---
+++ condicionales.py
@@ -0,0 +1,25 @@
+# condicional if
+# if condicion:
+    #bloque a ejecutar tabulado
+# else:
+    #bloque a ejecutar tabulado.
+
+#Condicional anidada
+
+"""
+if condicion:
+    #bloque a ejecutar tabulado
+if condicion:
+    #bloque a ejecutar tabulado
+
+En el caso anterior se validan todos los IF.
+
+
+
+if condicion:
+    #bloque a ejecutar tabulado
+elif condicion:
+    #bloque a ejecutar tabulado.
+
+    En el anterior caso si uno de los IF o elif es valido ya no se validan los demas.
+"""
